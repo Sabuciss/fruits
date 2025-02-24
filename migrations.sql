a76bb80bf22f2cceba0449c934b79cd35c3cb9f1
ipb23.s.brikmane <ipb23.s.brikmane@pjvt.local>
2025-02-24 10:53:23 +0200

    sql db

--- migrations.sql
+++ migrations.sql
@@ -1,31 +1,22 @@
+-- Dzēš esošo datu bāzi, ja tāda pastāv
 DROP DATABASE IF EXISTS fruits;
 CREATE DATABASE fruits;
 USE fruits;
 
-/* Izveido fruits tabulu */
+-- Izveido tabulu augļiem
 CREATE TABLE fruits (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    fruit_name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL
 );
 
-/* Ievieto kategorijas */
-INSERT INTO fruits (fruit_name) VALUES 
-("ābols"), 
-("banans"), 
-("ananas"); 
-
-/* Izveido posts tabulu ar saiti uz fruits */
-CREATE TABLE posts (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    content VARCHAR(5200) NOT NULL,
-    category_id INT NULL,
-    FOREIGN KEY (category_id) REFERENCES fruits(id) ON DELETE SET NULL
-);
-
-SELECT * FROM fruits;
-
-/* Ievieto bloga ierakstus */
-INSERT INTO posts (content) VALUES 
+-- Ievieto augļus
+INSERT INTO fruits (name) VALUES 
 ("ābols"), 
 ("banans"), 
 ("ananas");
+
+-- Pārskata visu informāciju no tabulas fruits
+SELECT * FROM fruits;
+
+-- Meklē augļus, kuru nosaukums satur "o"
+SELECT * FROM fruits WHERE name LIKE "%o%";
